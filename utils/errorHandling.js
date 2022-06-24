b07a401af744456a33616f3a503a33d104290e70
FarNys <faridsalmaniy@gmail.com>
2022-06-24 04:32:11 -0700

    +add simple err handling

--- utils/errorHandling.js
+++ utils/errorHandling.js
@@ -1,11 +1,45 @@
+const AppError = require("../utils/appError");
+
+const handleCastError = (err, res) => {
+  const message = `Invalid ${err.path}:${err.value}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("Error isNot Operational");
+    res.status(500).json({
+      status: "error",
+      message: "Not Operational Production",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "errOr";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    if (err.name === "CastError") error = handleCastError(error);
+
+    sendErrorProd(error, res);
+  }
 };
